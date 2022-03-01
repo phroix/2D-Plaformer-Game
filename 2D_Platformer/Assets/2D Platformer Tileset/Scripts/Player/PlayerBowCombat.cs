029a5e754f9fd730dd7c1cf8bd1b7b39b6c57fd7
phroix <94416893+phroix@users.noreply.github.com>
2022-03-01 08:50:02 +0100

    Player attacking with bow
    Player attacking with bow finished so far

---
+++ 2D_Platformer/Assets/2D Platformer Tileset/Scripts/Player/PlayerBowCombat.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBowCombat : MonoBehaviour
+{
+    public GameObject arrow;
+    public float normalAttackDamage = 0f;
+
+    public Transform shotpoint;
+    public LayerMask enemyLayers;
+
+    public float normalArrowSpeed = 100000f;
+    public float yAxis = .5f;
+
+    public float normalAttackRange = 0.5f;
+    public float normalAttackRate = 2f;
+
+    float nextAttackTime = 0f;
+    float nextMove = 0f;
+
+    //public float launchForce;
+
+    //Gameobject Components
+    Animator myAnimator;
+
+    //Parent Objects
+    GameObject parentObject;
+    Rigidbody2D parentRigidbody2D;
+    BoxCollider2D parentBoxCollider2D;
+    bool localScalel;
+    float xspeed;
+
+    Coroutine myCoroutine;
+
+
+
+    private void Awake()
+    {
+        parentObject = gameObject.transform.parent.gameObject;
+        parentRigidbody2D = parentObject.GetComponent<Rigidbody2D>();
+        parentBoxCollider2D = parentObject.GetComponent<BoxCollider2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetPlayerScale();
+        NormalAttack();
+    }
+
+    private void GetPlayerScale()
+    {
+        xspeed = parentRigidbody2D.transform.localScale.x;
+        if (xspeed == 1)
+        {
+            localScalel = true;
+        }
+        else if (xspeed == -1)
+        {
+            localScalel = false;
+        }
+    }
+
+    private void NormalAttack()
+    {
+        if (Time.time <= nextMove)
+        {
+            parentRigidbody2D.bodyType = RigidbodyType2D.Static;
+        }
+
+        if (Time.time >= nextAttackTime)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                //Make Player stand still
+                nextMove = Time.time + (1f / normalAttackRate);
+
+                //Play attack animation
+                myAnimator.SetTrigger("NormalAttack");          
+
+                nextAttackTime = Time.time + 1f / normalAttackRate;
+            }
+            else
+            {
+                parentRigidbody2D.bodyType = RigidbodyType2D.Dynamic;
+            }
+        }
+    }
+
+    private void InstatiateArrow()
+    {
+        GameObject newArrow = Instantiate(arrow, shotpoint.position, shotpoint.rotation) as GameObject;
+        //if(!localScalel) newArrow.transform.localScale *= -1;
+        newArrow.GetComponent<Rigidbody2D>().velocity = new Vector2(xspeed * normalArrowSpeed, yAxis);
+    }
+
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.DrawWireSphere(shotpoint.position, 0.1f);
+    //}
+}
