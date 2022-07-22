7cdb4fbdf52e027191e055906c31250fa5171425
phroix <94416893+phroix@users.noreply.github.com>
2022-07-23 01:45:41 +0200

    NPC system in progress

---
+++ 2D_Platformer/Assets/2D Platformer Tileset/Scripts/NPCs/ChildNPCWeaponSystem.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChildNPCWeaponSystem : MonoBehaviour
+{
+    public Text interactText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactText.text = "Welcome to the Weapon Shop!!";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+   
+}
