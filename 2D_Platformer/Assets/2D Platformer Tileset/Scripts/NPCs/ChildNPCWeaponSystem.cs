72a621659ef0e2a5fd26f813ce539c555fd07a9b
phroix <94416893+phroix@users.noreply.github.com>
2022-07-27 01:09:10 +0200

    NPC system in progress

--- 2D_Platformer/Assets/2D Platformer Tileset/Scripts/NPCs/ChildNPCWeaponSystem.cs
+++ 2D_Platformer/Assets/2D Platformer Tileset/Scripts/NPCs/ChildNPCWeaponSystem.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,17 +8,93 @@ public class ChildNPCWeaponSystem : MonoBehaviour
 {
     public Text interactText;
 
+    public GameObject swordFg;
+    public int sowrdCost = 75;
+    bool swordBuy = false;
+
+    public GameObject bowFg;
+    public int bowCost = 150;
+    bool bowBuy = false;
+
+    public GameObject arrowFg;
+    public int arrowCost = 2;
+    bool arrowBuy = false;
+
+    PlayerCoinController myPlayerCoinController;
+
     // Start is called before the first frame update
     void Start()
     {
-        interactText.text = "Welcome to the Weapon Shop!!";
+        myPlayerCoinController = FindObjectOfType<PlayerCoinController>();
+        interactText.text = "Weapon Shop";
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        BuyController();
     }
 
-   
+    private void BuyController()
+    {
+
+        if (swordBuy || sowrdCost > myPlayerCoinController.GetCurrentCoins())
+        {
+            swordFg.SetActive(true);
+        }
+        else
+        {
+            swordFg.SetActive(false);
+        }
+
+        if (bowBuy || bowCost > myPlayerCoinController.GetCurrentCoins())
+        {
+            bowFg.SetActive(true);
+        }
+        else
+        {
+            bowFg.SetActive(false);
+        }
+
+        if (arrowCost > myPlayerCoinController.GetCurrentCoins())
+        {
+            arrowFg.SetActive(true);
+        }
+        else
+        {
+            arrowFg.SetActive(false);
+        }
+    }
+
+    public void BuySword()
+    {
+        if(sowrdCost <= myPlayerCoinController.GetCurrentCoins() && !swordBuy)
+        {
+            myPlayerCoinController.DecreaseCurrentCoins(sowrdCost);
+            swordBuy = true;
+            swordFg.SetActive(true);
+        }
+    }
+
+    public void BuyBow()
+    {
+        if (bowCost <= myPlayerCoinController.GetCurrentCoins() && !bowBuy)
+        {
+            myPlayerCoinController.DecreaseCurrentCoins(bowCost);
+            bowBuy = true;
+            bowFg.SetActive(true);
+        }
+    }
+
+    public void BuyArrow()
+    {
+        if (arrowCost <= myPlayerCoinController.GetCurrentCoins() )
+        {
+            myPlayerCoinController.DecreaseCurrentCoins(arrowCost);
+        }
+    }
+
+
+
+
 }
